6ed2a27ccf2c9d1d7f20b9dab7bc64ea637eea74
Artorias <jennyArtorias@gmail.com>
2016-04-16 21:01:12 +1200

    bug:numbers replace in textbox addtoscreen()

---
+++ javascript.js
@@ -0,0 +1,19 @@
+var box = documet.getElementById("display");
+//Store no's
+
+
+//Function number for textbox
+
+function addtoscreen(x){
+    
+    box.value=x;
+    
+    
+    
+    
+    
+    
+}
+
+
+
